3ff299af24cd44f322e4ea0ea8d45f3452fd0d5a
Daniel Anah <1192daniel@gmail.com>
2022-06-29 17:56:52 -0700

    upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
     strup = ""
-    for x in range(len(str)):
-        if ord(str[x]) >= ord('a') and ord(str[x]) <= ord('z'):
-            strup = ord(str[x]) - 32
+    for i in range(len(str)):
+        if ord(str[i]) >= ord('a') and ord(str[i]) <= ord('z'):
+            strup = ord(str[i]) - 32
         else:
-            strup = ord(str[x])
+            strup = ord(str[i])
             print("{:c}".format(strup), end="")
         print()
