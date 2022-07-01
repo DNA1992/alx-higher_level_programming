6a8251bc60a08dce389f2e9c0d393c4f557ef7dc
Daniel Anah <1192daniel@gmail.com>
2022-07-01 07:52:54 -0700

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 from add_0 import add
 
-def main():
+def add():
     a = 1
     b = 2
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     return (a + b)
 
-    if __name__ == "__main__":
-        main()
+    if __name__ == "__add__":
+        add()
