c642ee431b1d17f2f0306d68febb0b8980d1936c
Daniel Anah <1192daniel@gmail.com>
2022-07-02 16:08:39 -0700

    divisible by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     div2 = []
     for x in my_list:
-        if i % 2 == 0:
+        if x % 2 == 0:
             div2.append(True)
         else:
             div2.append(False)
