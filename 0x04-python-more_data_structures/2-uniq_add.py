c465f11749bea56ba61f3c03746d66498757e62b
Daniel Anah <1192daniel@gmail.com>
2022-07-10 11:33:15 -0700

    uniq

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,8 +2,8 @@
 
 
 def uniq_add(my_list=[]):
-    x = 0
-    uniq = set(my_list)
-    for y in uniq:
-        x += y
-    return x
+    r = 0
+    uni = set(my_list)
+    for i in uni:
+        r += i
+    return r
