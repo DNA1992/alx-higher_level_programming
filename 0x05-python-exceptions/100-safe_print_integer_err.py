a6bb3cfe35cf7bd29c70627edf12c7e02ad7d5b2
Daniel Anah <1192daniel@gmail.com>
2022-07-18 18:06:58 -0700

    safe

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer_err(value):
+    p = False
+    m = "Exception: Unknown format code 'd' for object of type 'str'\n"
+    try:
+        print("{:d}".format(value))
+        p = True
+    except (ValueError, TypeError) as m:
+        print("Exception: {}".format(m), file=sys.stderr)
+    return 
